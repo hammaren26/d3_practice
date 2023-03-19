9fbbc96c6d7422b98c63dad14e7e8215d9f8461f
Vyacheslav Tulaev <hammaren23@gmail.com>
2023-03-19 19:21:23 +0600

    Диаграмма рассеивания

--- src/debugJS.js
+++ src/debugJS.js
@@ -8,190 +8,46 @@ import colorbrewer from 'colorbrewer';
 import { ConvertCsvToArr, highlightRegion } from './functions';
 
 
-
-function createSoccerViz() {
-   function overallTeamViz(incomingData) {
-      d3.select("svg")
-         .append("g")
-         .attr("id", "teamsG")
-         .attr("transform", "translate(50,300)").selectAll("g")
-         .data(incomingData)
-         .enter()
-         .append("g")
-         .attr("class", "overallG")
-         .attr("transform", function (d, i) {
-            return `translate(${i * 90}, 0)`
-         })
-
-      let teamG = d3.selectAll("g.overallG");
-
-      teamG
-         .append("circle")
-         .attr("r", 20)
-
-      teamG
-         .append("text")
-         .attr("y", 30)
-         .attr("x", -30)
-         .text(d => d.team)
-
-      // d3.selectAll("g.overallG")
-      //    .insert("image", "text")
-      //    .attr("href", d => `images/${d.team}.png`)
-      //    .attr("width", "45px")
-      //    .attr("height", "20px")
-      //    .attr("x", -22)
-      //    .attr("y", -10)
-
-      teamG.on("mouseenter", highlightRegion);
-      teamG.on("mouseleave", function () {
-         d3.selectAll("g.overallG")
-            .select("circle")
-            .attr("class", "")
-
-         d3.selectAll("g.overallG")
-            .select("text")
-            .classed("active", false)
-            .transition()
-            .duration(300)
-            .attr("y", 30)
-            .attr("style", "transform: scale(1)")
-      })
-
-
-      d3.text("resources/infobox.html").then(data => {
-         d3.select("body")
-            .append("div")
-            .attr("id", "infobox")
-            .html(data)
-      })
-
-
-      function loadSVG(svgData) {
-         console.log(svgData)
-
-
-         d3.select(svgData).selectAll("path").each(function () {
-            d3.select("svg").node().appendChild(this);
-         });
-
-
-
-
-
-         d3.selectAll("path").attr("transform", "translate(50,50)");
-      }
-
-
-
-      d3.html("ball.svg").then(data => {
-         loadSVG(data)
-      })
-
-
-
-
-      teamG.on("click", teamClick)
-
-
-      function teamClick(event) {
-         d3.selectAll("td.data")
-            .data(Object.values(event.target.__data__))
-            .html(p => {
-               return p;
-            })
-      }
-
-
-
-      function highlightRegion(event) {
-         // d3.selectAll("g.overallG")
-         //    .select("circle")
-         //    .attr("class", p => {
-         //       return p.region === event.target.__data__.region ? "active" : "inactive"
-         //    })
-
-         d3.select(this)
-            .select("text")
-            .classed("active", true)
-            .transition()
-            .duration(300)
-            .attr("y", 10)
-            .attr("style", "transform: scale(1.5)")
-
-         d3.selectAll("g.overallG")
-            .select("circle")
-            .each(function (p) {
-               p.region == event.target.__data__.region ?
-                  d3.select(this).classed("active", true) :
-                  d3.select(this).classed("inactive", true)
-            })
-
-         let teamColor = d3.rgb("#75739F");
-
-         d3.selectAll("g.overallG")
-            .select("circle")
-            .style("fill", (p) => {
-               return p.region === event.target.__data__.region ? teamColor.darker(.75) : teamColor.brighter(.5)
-            })
-      }
-
-      const dataKeys = Object.keys(incomingData[0])
-         .filter(d => {
-            return d != false && d !== "team" && d !== "region"
-         });
-
-      d3.select("#controls")
-         .selectAll("button.teams")
-         .data(dataKeys)
-         .enter()
-         .append("button")
-         .on("click", buttonClick)
-         .html(d => d);
-
-      function buttonClick(event) {
-         let maxValue = d3.max(incomingData, d => parseFloat(d[event.target.innerHTML]))
-         let colorQuantize = d3.scaleQuantize()
-            .domain([0, maxValue])
-            .range(colorbrewer.Greens[3]);
-         let tenColorScale = d3.scaleOrdinal()
-            .domain(["UEFA", "CONMEBOL", "CAF"])
-            .range(d3.schemeCategory10)
-            .unknown('#c4b9ac')
-         let radiusScale = d3.scaleLinear().domain([0, maxValue]).range([2, 20]);
-
-         // d3.selectAll("g.overallG")
-         //    .select("circle")
-         //    .transition()
-         //    .duration(1000)
-         // .attr("r", d => {
-         //    return radiusScale(d[event.target.innerHTML])
-         // })
-
-         let ybRamp = d3
-            .scaleLinear()
-            .interpolate(d3.interpolateLab)
-            .domain([0, maxValue])
-            .range(["blue", "yellow"])
-
-
-         d3.selectAll("g.overallG")
-            .select("circle")
-            .transition()
-            .duration(1000)
-            // .attr("r", d => radiusScale(d[event.target.innerHTML]))
-            // .style("fill", d => tenColorScale(d.region))
-            .style("fill", d => colorQuantize(d[event.target.innerHTML]))
-            .attr("r", d => radiusScale(d[event.target.innerHTML]))
-      }
-   }
-
-   ConvertCsvToArr("worldcup.csv").then(data => {
-      overallTeamViz(data)
-   })
-}
-
-
 document.addEventListener('DOMContentLoaded', function (e) {
-   createSoccerViz()
+
+
+   let scatterData = [
+      { friends: 5, salary: 22000 },
+      { friends: 3, salary: 18000 },
+      { friends: 10, salary: 88000 },
+      { friends: 0, salary: 180000 },
+      { friends: 27, salary: 56000 },
+      { friends: 8, salary: 74000 }
+   ];
+
+   let xExtent = d3.extent(scatterData, d => d.salary);
+   let yExtent = d3.extent(scatterData, d => d.friends);
+   let xScale = d3.scaleLinear().domain(xExtent).range([0, 500]);
+   let yScale = d3.scaleLinear().domain(yExtent).range([0, 500]);
+
+
+   console.log({
+      xExtent,
+      yExtent,
+      xScale,
+      yScale,
+   });
+
+   d3.select("svg")
+      .selectAll("circle")
+      .data(scatterData)
+      .enter()
+      .append("circle")
+      .attr("r", 5)
+      .attr("cx", d => xScale(d.salary))
+      .attr("cy", d => yScale(d.friends));
+
+
+   let yAxis = d3.axisRight().scale(yScale).ticks(4).tickSize(500)
+   d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis)
+
+   let xAxis = d3.axisBottom().scale(xScale).tickSize(500).ticks(8)
+   d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis)
+   // xAxis(d3.select("svg").append("g").attr("id", "xAxisG"))
+
 })
