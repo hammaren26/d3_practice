40befb4d76a068e258b107a52121db36cbb2c002
Vyacheslav Tulaev <hammaren23@gmail.com>
2023-04-08 19:33:36 +0600

    Я нихуя не понял, как сделать плавный переход без сортировки (в уч. стр 154.) Как-нибудь сесть и под пивко еще раз попробовать

--- src/debugJS.js
+++ src/debugJS.js
@@ -1,6 +1,7 @@
-import { reverse, selection } from 'd3';
-import { select, range, nest } from 'd3';
+
 import * as d3 from "d3";
+import { reverse, selection, scaleLinear } from 'd3';
+import { select, range } from 'd3';
 import $ from "jquery";
 import jQuery from "jquery";
 import inputmask from "inputmask/dist/jquery.inputmask.js";
@@ -8,58 +9,146 @@ import colorbrewer from 'colorbrewer';
 import { ConvertCsvToArr, highlightRegion } from './functions';
 
 
-document.addEventListener('DOMContentLoaded', function (e) {
 
+
+document.addEventListener('DOMContentLoaded', function (e) {
    d3.json("tweets.json").then(data => {
-      histogram(data.tweets)
+      pieChart(data.tweets)
+      // test()
    })
 
+   function pieChart(data) {
+      var nestedTweets = d3.group(data, d => d.user)
+
+      nestedTweets.forEach(obj => {
+         obj.numTweets = obj.length;
+         obj.numFavorites = d3.sum(obj, p => {
+            return p.favorites.length
+         })
+         obj.numRetweets = d3.sum(obj, p => {
+            return p.retweets.length
+         })
+      })
+
+      let pieChart = d3.pie();
+      let newArc = d3.arc();
+      pieChart.value(d => d[1].numTweets).sort(null)
+      let tweetsPie = pieChart(nestedTweets)
 
 
-   function histogram(tweetsData) {
 
-      var fillScale = d3.scaleOrdinal().range(["#fcd88a", "#cf7c1c", "#93c464"])
-      var normal = d3.randomNormal()
-      var sampleData1 = d3.range(100).map(d => normal())
-      var sampleData2 = d3.range(100).map(d => normal())
+      pieChart.value(d => {
+         return d[1].numRetweets
+      })
 
-      console.log(sampleData2);
+      let retweetsPie = pieChart(nestedTweets)
 
-      var sampleData3 = d3.range(100).map(d => normal())
-      var histoChart = d3.histogram();
 
-      histoChart
-         .domain([-3, 3])
-         .thresholds([-3, -2.5, -2, -1.5, -1,
-         -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3])
-         .value(d => d)
 
-      var yScale = d3.scaleLinear().domain([-3, 3]).range([400, 0]);
-      var yAxis = d3.axisRight().scale(yScale)
-         .tickSize(300)
+      nestedTweets.forEach((d, i) => {
+         d.tweetsSlice = tweetsPie.filter(el => {
+            return el['data'][0] == i;
+         })
+         d.retweetsSlice = retweetsPie.filter(el => {
+            return el['data'][0] == i;
+         })
+      })
 
-      d3.select("svg").append("g").call(yAxis)
+      // console.log('nestedTweets', nestedTweets);
+      // console.log('tweetsPie', tweetsPie);
+      // console.log(retweetsPie);
 
-      var area = d3.area()
-         .x0(d => -d.length)
-         .x1(d => d.length)
-         .y(d => yScale(d.x0))
-      // .curve(d3.curveCatmullRom)
+
+
+
+
+
+
+      newArc
+         .innerRadius(20)
+         .outerRadius(100)
+
+      var fillScale = d3.scaleOrdinal()
+         .range(["#fcd88a", "#cf7c1c", "#93c464", "#75734F"])
 
       d3.select("svg")
-         .selectAll("g.violin")
-         .data([sampleData1, sampleData2, sampleData3])
-         .enter()
          .append("g")
-         .attr("class", "violin")
-         .attr("transform", (d, i) => `translate(${50 + i * 100},0)`)
+         .attr("transform", "translate(100,100)")
+         .selectAll("path")
+         .data(tweetsPie, function (d, i) {
+            // console.log(d, i);
+         })
+         .enter()
          .append("path")
-         .style("stroke", "black")
+         .attr("d", newArc)
          .style("fill", (d, i) => fillScale(i))
-         .attr("d", d => area(histoChart(d)))
+         .style("stroke", "black")
+         .style("stroke-width", "2px");
+
+
+      btn.addEventListener('click', () => {
+         pieChart.value(d => d[1].numFavorites).sort(null)
+
+         d3.selectAll("path")
+            .data(pieChart(nestedTweets))
+            .transition()
+            .duration(1000)
+            .attr("d", newArc)
+         // .attrTween("d", arcTween)
+      })
+
+      function arcTween(d) {
+         console.log(d);
+         return t => {
+            // var interpolateStartAngle = d3
+            //    .interpolate(d.tweetsSlice.startAngle, d.retweetsSlice.startAngle);
+            // var interpolateEndAngle = d3
+            //    .interpolate(d.tweetsSlice.endAngle, d.retweetsSlice.endAngle);
+            // d.startAngle = interpolateStartAngle(t);
+            // d.endAngle = interpolateEndAngle(t);
+            return newArc(d);
+         }
+      }
    }
 
+   function test() {
+      // const i = d3.interpolateNumber(0, 100);
+      const i = d3.interpolate(
+         { colors: ["red", "blue"] },
+         { colors: ["white", "black"] }
+      );
+      let ff = i(1)
+      console.log(ff);
+      // let f = d3.interpolateLab("#ffffff", "red")(0.3);
+      // console.log(f)
 
 
 
+      btn.addEventListener('click', () => {
+
+
+
+         console.log(d3.select("path#test"));
+
+         d3.select("path#test")
+            .transition()
+            .duration(5000)
+            // .style("fill", 'black')
+            .styleTween("fill", function (t) {
+               console.log(t);
+               return function (t) {
+                  return "hsl(" + t * 360 + ",100%,50%)";
+               };
+            })
+
+
+
+      })
+
+
+
+
+
+
+   }
 })
